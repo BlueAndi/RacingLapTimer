f971911958a9f6e21e4af0021c109a671eec783b
BlueAndi <web@blue-andi.de>
2020-07-13 21:26:26 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# RacingLapTimer
-Racing lap timer, based on a laser sensor.
+# Racing Lap Timer
+A racing lap timer, based on a laser sensor.
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](http://choosealicense.com/licenses/mit/)
+[![Repo Status](https://www.repostatus.org/badges/latest/wip.svg)](https://www.repostatus.org/#wip)
+[![Release](https://img.shields.io/github/release/BlueAndi/RacingLapTimer.svg)](https://github.com/BlueAndi/RacingLapTimer/releases)
+
+- [Racing Lap Timer](#racing-lap-timer)
+- [Motivation](#motivation)
+- [Electronic](#electronic)
+- [Software](#software)
+  - [Preparation](#preparation)
+  - [IDE](#ide)
+  - [Installation](#installation)
+  - [Build Project](#build-project)
+  - [Update of the device](#update-of-the-device)
+    - [Update via usb](#update-via-usb)
+  - [Used Libraries](#used-libraries)
+- [Issues, Ideas And Bugs](#issues-ideas-and-bugs)
+- [License](#license)
+
+# Motivation
+For line follower competitions a racing lap timer shall be used. It doesn't only measure the lap time, it shows it nice via web interface, inclusive automatically result table generation.
+
+# Electronic
+
+* [Wemos D1 Mini (esp8266)](https://docs.platformio.org/en/latest/boards/espressif8266/d1_mini.html)
+* [Waveshare laser sensor](www.waveshare.com/wiki/Laser_Sensor)
+
+# Software
+
+## Preparation
+Install the serial driver for the Wemos D1 Mini board. Find the latest drivers here: https://www.wemos.cc/en/latest/ch340_driver.html
+
+## IDE
+The [PlatformIO IDE](https://platformio.org/platformio-ide) is used for the development. Its build on top of Microsoft Visual Studio Code.
+
+## Installation
+1. Install [VSCode](https://code.visualstudio.com/).
+2. Install PlatformIO IDE according to this [HowTo](https://platformio.org/install/ide?install=vscode).
+3. Close and start VSCode again.
+4. Recommended is to take a look to the [quick-start guide](https://docs.platformio.org/en/latest/ide/vscode.html#quick-start).
+
+## Build Project
+1. Load workspace in VSCode.
+2. Change to PlatformIO toolbar.
+3. _Project Tasks -> Build_ or via hotkey ctrl-alt-b
+
+## Update of the device
+
+### Update via usb
+1. Connect the Wemos D1 Mini board to your PC via USB.
+2. Build and upload the software via _Project Tasks -> Upload_.
+3. Build and upload the filesystem via _Project Tasks -> Upload File System image_.
+
+## Used Libraries
+* [Arduino](https://github.com/esp8266/Arduino) - ESP framework.
+* [WifiManager](https://github.com/tzapu/WiFiManager) - ESP8266 WiFi Connection manager with fallback web configuration portal.
+* [WebSockets](https://github.com/Links2004/arduinoWebSockets) - A WebSocket Server and Client for Arduino based on RFC6455.
+
+# Issues, Ideas And Bugs
+If you have further ideas or you found some bugs, great! Create a [issue](https://github.com/BlueAndi/RacingLapTimer/issues) or if you are able and willing to fix it by yourself, clone the repository and create a pull request.
+
+# License
+The whole source code is published under the [MIT license](http://choosealicense.com/licenses/mit/).
---
+++ data/web/ws.js
@@ -0,0 +1,157 @@
+var cpjs = cpjs | {};
+
+cpjs = {
+    ws: {}
+};
+
+cpjs.ws.Client = function(options) {
+
+    this.socket     = null;
+    this.cmdQueue   = [];
+    this.pendingCmd = null;
+    this.onEvent    = null;
+
+    this._sendCmdFromQueue = function() {
+        var msg = "";
+
+        if (0 < this.cmdQueue.length) {
+            this.pendingCmd = this.cmdQueue.shift();
+
+            msg = this.pendingCmd.name;
+
+            if (null !== this.pendingCmd.par) {
+                msg += ";" + this.pendingCmd.par;
+            }
+
+            console.info("Websocket command: " + msg);
+            this.socket.send(msg);
+        }
+    };
+
+    this._sendCmd = function(cmd) {
+
+        this.cmdQueue.push(cmd);
+
+        if (null === this.pendingCmd) {
+            this._sendCmdFromQueue();
+        }
+    };
+
+    this._sendEvt = function(evt) {
+        if (null !== this.onEvent) {
+            this.onEvent(evt);
+        }
+    }
+};
+
+cpjs.ws.Client.prototype.connect = function(options) {
+    return new Promise(function(resolve, reject) {
+        var wsUrl;
+
+        if (("string" !== typeof options.protocol) ||
+            ("string" !== typeof options.hostname) ||
+            ("number" !== typeof options.port) ||
+            ("string" !== typeof options.endpoint)) {
+
+            reject();
+        } else {
+
+            if ("function" === typeof options.onEvent) {
+                this.onEvent = options.onEvent;
+            }
+
+            try {
+                wsUrl = options.protocol + "://" + options.hostname + ":" + options.port + options.endpoint;
+                this.socket = new WebSocket(wsUrl);
+
+                this.socket.onopen = function(openEvent) {
+                    console.debug("Websocket opened.");
+                    resolve(this);
+                }.bind(this);
+
+                this.socket.onclose = function(closeEvent) {
+                    console.debug("Websocket closed.");
+                    if ("function" === typeof options.onClosed) {
+                        options.onClosed();
+                    }
+
+                    options.evtCallback = null;
+                };
+
+                this.socket.onmessage = function(messageEvent) {
+                    console.debug("Websocket message: " + messageEvent.data);
+                    this._onMessage(messageEvent.data);
+                }.bind(this);
+
+            } catch (exception) {
+                console.error(exception);
+                options.evtCallback = null;
+                reject();
+            }
+        }
+    }.bind(this));
+};
+
+cpjs.ws.Client.prototype._onMessage = function(msg) {
+    var data    = msg.split(";");
+    var status  = data.shift();
+    var rsp     = {};
+    var index   = 0;
+
+    if ("EVT" === status) {
+        if (0 < data.length) {
+            rsp.event = data[0];
+
+            if ("STARTED" == rsp.event) {
+                /* No further parameter */
+            } else if ("FINISHED" == rsp.event) {
+                rsp.duration = parseInt(data[1]);
+            } else {
+                console.error("Unknown event: " + rsp.event);
+            }
+        } else {
+            console.error("Unknown empty event.");
+        }
+
+        this._sendEvt(rsp);
+    } else {
+        if (null === this.pendingCmd) {
+            console.error("No pending command, but response received.");
+        } else if ("ACK" === status) {
+            if ("RELEASE" === this.pendingCmd.name) {
+                rsp.data = [];
+                for(index = 0; index < data.length; ++index) {
+                    rsp.data.push(parseInt(data[index], 16));
+                }
+                this.pendingCmd.resolve(rsp);
+            } else {
+                console.error("Unknown command: " + this.pendingCmd.name);
+                this.pendingCmd.reject();
+            }
+        } else {
+            console.error("Command " + this.pendingCmd.name + " failed.");
+            this.pendingCmd.reject();
+        }
+
+        this.pendingCmd = null;
+    }
+
+    this._sendCmdFromQueue();
+
+    return;
+};
+
+cpjs.ws.Client.prototype.release = function() {
+    return new Promise(function(resolve, reject) {
+        if (null === this.socket) {
+            reject();
+        } else {
+            this._sendCmd({
+                name: "RELEASE",
+                par: null,
+                resolve: resolve,
+                reject: reject
+            });
+        }
+    }.bind(this));
+};
